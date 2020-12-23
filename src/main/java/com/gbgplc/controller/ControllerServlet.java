1a43b78dd571a5c372c90b732a609425949c8a41
jcogolludo <JoseManuel.PerezCogolludo@gbgplc.com>
2020-12-23 11:25:03 +0100

    fixing SQ issues

--- src/main/java/com/gbgplc/controller/ControllerServlet.java
+++ src/main/java/com/gbgplc/controller/ControllerServlet.java
@@ -2,7 +2,6 @@ package com.gbgplc.controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Random;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -16,48 +15,21 @@ public class ControllerServlet extends HttpServlet {
 	 */
 	private static final long serialVersionUID = 1L;
 	
-	String response;	
-	
-	
-	@Override
-	public void init() throws ServletException {		
-		super.init();		
-	}
-
-
-
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		int max = 30000;
-		int min = 5000;
-		try {
-			Thread.sleep(getRandomDelay(min, max));
-		} catch (Exception e) {
-			e.printStackTrace();
-			throw new ServletException(e);
-		}
-		
-		resp.setContentType("text/xml");
-		
-		PrintWriter out = resp.getWriter();
-	    out.println(response);
+		doGet(req,resp);
 	}
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		resp.setContentType("text/html");
 		
-		PrintWriter out = resp.getWriter();
-	    out.println("OK");
-	}
-	
-	/**
-	 * Generates a random integer between 10000 and 30000
-	 * @return
-	 */
-	private long getRandomDelay(int min, int max) {		
-		Random r = new Random();
-		return r.nextInt((max-min) + 1) + min;
+		try {
+			PrintWriter out = resp.getWriter();
+		    out.println("OK");		    
+		} catch (Exception e) {
+			System.err.print("KO");  
+		}
 	}
 	
 }
